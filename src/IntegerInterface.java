535960a9bde3909dd2ce0458134784e83e1936c5
Andrii Palamar <palamar.andrii@chnu.edu.ua>
2023-06-06 08:33:55 +0100

    21

--- src/IntegerInterface.java
+++
@@ -1,6 +0,0 @@
-public interface IntegerInterface {
-    IntegerInterface add(IntegerInterface other);
-    IntegerInterface subtract(IntegerInterface other);
-    IntegerInterface multiply(IntegerInterface other);
-    boolean equals(Object obj);
-}
